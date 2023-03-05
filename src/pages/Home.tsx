0bc87ca044d418c1f100abb69d43ded075afc528
Nguyễn Tiến Cường <cuong.nguyen@apps-cyclone.com>
2023-03-05 14:39:19 +0700

    Update home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 
 interface RootState {
@@ -6,16 +7,27 @@ interface RootState {
   };
 }
 
+interface Ref {
+  focus: () => {};
+}
 const HomePage = () => {
   const homeState = useSelector((state: RootState) => state.home);
-  console.log(homeState);
+  const ref = useRef(null);
+  useEffect(() => {
+    console.log("mounted");
+  }, [homeState]);
   return (
-    <div className="flex justify-center items-center h-[100vh] min-w-[390px]">
-      <div className="w-32 h-32 bg-slate-400 m-2 flex justify-center items-center">
-        farm
-      </div>
-      <div className="w-32 h-32 bg-slate-400 m-2 flex justify-center items-center">
-        game
+    <div
+      id="bg"
+      className="bg-[url('https://res.cloudinary.com/cuong/image/upload/v1677939051/avatar/bg-f_dy9g9t.jpg')] bg-no-repeat bg-cover bg-center "
+    >
+      <div className="flex justify-center items-center h-[100vh] min-w-[390px] ">
+        <div className="w-32 h-32 bg-slate-400 m-2 flex justify-center items-center">
+          farm
+        </div>
+        <div className="w-32 h-32 bg-slate-400 m-2 flex justify-center items-center">
+          game
+        </div>
       </div>
     </div>
   );
